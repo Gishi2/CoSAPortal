f895fdef88f726aa07a154a54f70938eb45e7c0d
Nuranishah <kazimahanishah@gmail.com>
2023-12-19 09:42:37 +0800

    Added Sign Up

--- Login system/signup.php
+++ Login system/signup.php
@@ -51,10 +51,36 @@ if(isset($_POST['submit'])){
 
 <body>
 
-    <header>
-        <a href="homepage.html" class="navbar-brand d-flex align-items-center px-4">
-            <img src="/Homepage/img/cosa/cosa_logo_inBlue.png">
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <img class="header-logo" src="/Homepage/img/cosa/cosa_logo_inBlue.png">
+            <!-- <h1 class="m-0 text-primary">PORTAL</h1> -->
+            <!-- <h1 class="m-0 text-primary"><i class="far fa-hospital me-3"></i>Klinik</h1> -->
         </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.php" class="nav  -item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <!-- <a href="service.html" class="nav-item nav-link">Service</a> -->
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu rounded-0 rounded-bottom m-0">
+                        <!-- <a href="about.html" class="dropdown-item">About Us</a> -->
+                        <a href="\Homepage\programme-registration-user.html" class="dropdown-item">Programme</a>
+                        <a href="\Merchandise\merchandise.html" class="dropdown-item">Merchandise</a>
+                        <a href="\CoSA E-Book\ebook.html" class="dropdown-item">E-Book</a>
+                    </div>
+                </div>
+                <!-- <a href="contact.html" class="nav-item nav-link">Contact</a> -->
+            </div>
+            <a href="/sign-up/signup.html" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Login / Sign Up<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
          <nav class="navigation">
             <a href="\CoSAPortal\Homepage\homepage.html">Home</a>
             <a href="#">About</a>
