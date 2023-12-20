4842ea38dad4ea2ef8b43d253ddaa1e1fb4c45f3
minemchet <chadisaiah17@gmail.com>
2023-12-20 19:38:35 +0800

    linked index/login/signup

--- Login-system/login.php
+++ Login-system/login.php
@@ -1,6 +1,6 @@
 <?php
 
-@include 'config.php';
+include 'config.php';
 
 if(isset($_POST['submit'])) {
    $username = mysqli_real_escape_string($conn, $_POST['username']);
@@ -74,7 +74,7 @@ if(isset($_POST['submit'])) {
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
-        <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <a href="/index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <img class="header-logo" src="/CoSAPortal/Homepage/img/cosa/cosa_logo_inBlue.png">
             <!-- <h1 class="m-0 text-primary">PORTAL</h1> -->
             <!-- <h1 class="m-0 text-primary"><i class="far fa-hospital me-3"></i>Klinik</h1> -->
@@ -84,11 +84,11 @@ if(isset($_POST['submit'])) {
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav  -item nav-link active">Home</a>
+                <a href="/index.php" class="nav  -item nav-link">Home</a>
                 <a href="about.html" class="nav-item nav-link">About</a>
                 <!-- <a href="service.html" class="nav-item nav-link">Service</a> -->
                 <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" style ="margin-right:70px">Pages</a>
                     <div class="dropdown-menu rounded-0 rounded-bottom m-0">
                         <!-- <a href="about.html" class="dropdown-item">About Us</a> -->
                         <a href="\Homepage\programme-registration-user.html" class="dropdown-item">Programme</a>
@@ -98,7 +98,7 @@ if(isset($_POST['submit'])) {
                 </div>
                 <!-- <a href="contact.html" class="nav-item nav-link">Contact</a> -->
             </div>
-            <a href="/sign-up/signup.html" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Login / Sign Up<i class="fa fa-arrow-right ms-3"></i></a>
+            <!-- <a href="/sign-up/signup.html" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Login / Sign Up<i class="fa fa-arrow-right ms-3"></i></a> -->
         </div>
     </nav>
     <!-- Navbar End -->
