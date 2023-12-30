c632ec0ad0f8d1d102dc65cb2776caef8d1f787a
Nuranishah <kazimahanishah@gmail.com>
2023-12-30 16:33:49 +0800

    Added Login

--- Login-system/login.php
+++ Login-system/login.php
@@ -1,32 +1,45 @@
 <?php
 
+session_start();
+
 @include 'config.php';
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
    $username = mysqli_real_escape_string($conn, $_POST['username']);
-   $pass = md5($_POST['password']);
+   $password = $_POST['password'];
 
-   $select = "SELECT * FROM user WHERE username ='$username' && userPassword = '$pass' ";
-   
-   $result = mysqli_query($conn, $select);
+   $select = "SELECT * FROM user WHERE username = ?";
 
-   if(mysqli_num_rows($result) > 0) {
-    
-    $row = mysqli_fetch_array($result);
+   $stmt = $conn->prepare($select);
+   $stmt->bind_param("s", $username);
+   $stmt->execute();
+   $result = $stmt->get_result();
 
-    if($row['user_type'] == 'admin') {
-        $_SESSION['admin _name'] = $row['name'];
-        header('location:admin.php');
-    } elseif ($row['user_type'] == 'user'){
-        $_SESSION['user_name'] = $row['name'];
-        header('location:user.php');
-     }
-      
+   if ($result->num_rows > 0) {
+      $row = $result->fetch_assoc();
+
+      if (password_verify($password, $row['userPassword'])) {
+         if ($row['user_type'] == 'admin') {
+            $_SESSION['admin_name'] = $row['name'];
+            header('location: admin.php');
+            exit();
+         } elseif ($row['user_type'] == 'user') {
+            $_SESSION['user_name'] = $row['name'];
+            header('location: user.php');
+            exit();
+         }
+      } else {
+         $error[] = 'Incorrect Username or Password!';
+      }
    } else {
-    $error[] = 'Incorrect Username or Password!';
+      $error[] = 'Incorrect Username or Password!';
    }
+
+   $stmt->close();
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
